ea6ddf535086b7be252c23d083653e69cf896e49
youssefkhalil320 <91757835+youssefkhalil320@users.noreply.github.com>
2021-10-01 21:34:16 +0200

    Add files via upload

---
+++ Security.py
@@ -0,0 +1,52 @@
+import cv2
+import os
+from tkinter import font
+
+cascPath=os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
+cascPath2=os.path.dirname(cv2.__file__)+"/data/haarcascade_fullbody.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+bodyCascade = cv2.CascadeClassifier(cascPath2)
+
+video_capture = cv2.VideoCapture(0)
+
+
+
+
+
+
+while True:
+    # Capture frame-by-frame
+    ret, frames = video_capture.read()
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    bodies = bodyCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    cv2.putText(frames, f"Number of ppl in the room is: {len(faces)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+  
+
+    
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display the resulting frame
+    cv2.imshow('Video', frames)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+
+
